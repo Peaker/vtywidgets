c850a4e4dbd4f85ebf387e215483676cfc2f75ed
Eyal Lotem <eyal.lotem@gmail.com>
2010-04-29 15:20:16 +0300

    Bar: Fix start..end range

--- src/Graphics/UI/VtyWidgets/Bar.hs
+++ src/Graphics/UI/VtyWidgets/Bar.hs
@@ -10,18 +10,22 @@ import Graphics.UI.VtyWidgets.Vector2(Vector2(..))
 import qualified Graphics.UI.VtyWidgets.TermImage as TermImage
 import qualified Graphics.UI.VtyWidgets.Widget as Widget
 
-validateRatio :: Double -> Double
-validateRatio = max 0 . min 1
+normalizeRange :: Double -> Double -> (Double, Double)
+normalizeRange low high = (low', high')
+  where
+    low' = max 0 . min 1 $ low
+    high' = max 0 . min 1 . max low' $ high
 
 makeHorizontal :: Vty.Attr -> Int -> Double -> Double -> Widget.Display a
 makeHorizontal attr minWidth start end =
   Widget.Display (Widget.horizontallyExpanding height minWidth) mkImage
   where
     height = 1
+    (start', end') = normalizeRange start end
     mkImage _ (Vector2 size _) = TermImage.string attr text
       where
-        before = truncate . (* fromIntegral size) . validateRatio $ start
-        after = truncate . (* fromIntegral size) . (1 -) . validateRatio $ end
+        before = truncate . (* fromIntegral size) $ start'
+        after = truncate . (* fromIntegral size) . (1 -) $ end'
         inside = size - (before + after)
         text = concat [
           replicate before '=',
@@ -34,10 +38,11 @@ makeVertical attr minHeight start end =
   Widget.Display (Widget.verticallyExpanding width minHeight) mkImage
   where
     width = 1
+    (start', end') = normalizeRange start end
     mkImage _ (Vector2 _ size) = TermImage.string attr text
       where
-        before = truncate $ fromIntegral size * start
-        after = truncate $ fromIntegral size * (1-end)
+        before = truncate . (*fromIntegral size) $ start'
+        after = truncate . (*fromIntegral size) . (1-) $ end'
         inside = size - (before + after)
         text = intersperse '\n' . concat $ [
           replicate before '=',
