37577ae2a51a4a7745ed0ee2fd7c310b0d7c2e43
Eyal Lotem <eyal.lotem@gmail.com>
2010-06-04 22:24:18 +0300

    Add Align widget

---
+++ src/Graphics/UI/VtyWidgets/Align.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS -O2 -Wall #-}
+
+module Graphics.UI.VtyWidgets.Align
+    (make)
+where
+
+import Control.Applicative(liftA2)
+import Data.Vector.Vector2(Vector2)
+import Graphics.UI.VtyWidgets.SizeRange(SizeRange(..))
+import Graphics.UI.VtyWidgets.Display(Display)
+import qualified Graphics.UI.VtyWidgets.Placable as Placable
+import qualified Graphics.UI.VtyWidgets.TermImage as TermImage
+
+type Alignment = Vector2 Double
+
+make :: Alignment -> Display a -> Display a
+make alignment display = Placable.atPlace alignImage display
+  where
+    maxSize = srMaxSize . Placable.pRequestedSize $ display
+    alignImage mkImage givenSize = TermImage.translate translation . mkImage size
+      where
+        translation = fmap round . liftA2 (*) alignment . fmap fromIntegral $ extraSize
+        extraSize = liftA2 (-) givenSize size
+        size = liftA2 min givenSize maxSize
