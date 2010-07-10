1a1920a97d7231592ff7fde56422d17ca359a285
Eyal Lotem <eyal.lotem@gmail.com>
2010-07-10 10:16:09 +0300

    TableGrid: Replace ljustify with a change of requested size and TermImage-based fill of attribute. Handle newlines in split to width.

--- src/Graphics/UI/VtyWidgets/TableGrid.hs
+++ src/Graphics/UI/VtyWidgets/TableGrid.hs
@@ -4,40 +4,48 @@ module Graphics.UI.VtyWidgets.TableGrid
     (makeColumnView, makeKeymapView)
 where
 
+import Control.Applicative(pure)
+import Control.Arrow(first)
+import Data.Monoid(mappend)
 import Data.Ord(comparing)
 import Data.List(sortBy, intercalate)
-import Data.List.Split(splitEvery)
+import Data.List.Split(splitEvery, splitOn)
 import Data.Vector.Vector2(Vector2(..))
+import qualified Data.Vector.Vector2 as Vector2
+import Data.Vector.Rect(Rect(..))
 import qualified Data.Map as Map
 import qualified Graphics.Vty as Vty
+import qualified Graphics.UI.VtyWidgets.TermImage as TermImage
+import qualified Graphics.UI.VtyWidgets.Placable as Placable
 import Graphics.UI.VtyWidgets.Keymap(Keymap(keymapGroups))
 import qualified Graphics.UI.VtyWidgets.Align as Align
 import qualified Graphics.UI.VtyWidgets.Grid as Grid
+import qualified Graphics.UI.VtyWidgets.SizeRange as SizeRange
 import Graphics.UI.VtyWidgets.Display(Display)
 import qualified Graphics.UI.VtyWidgets.TextView as TextView
 
-makeColumnView :: [Vty.Attr] -> [[String]] -> Display a
+fitToWidth :: Int -> String -> String
+fitToWidth width = intercalate "\n" . concatMap (splitEvery width) . splitOn "\n"
+
+makeColumnView :: [(Vty.Attr, Int)] -> [[String]] -> Display a
 makeColumnView attrs table =
   Grid.makeView $ (map . zipWith f) attrs table
   where
-    f attr x = Align.to (Vector2 0 0.5) . TextView.make attr $ x
-
-ljustify :: Int -> a -> [a] -> [a]
-ljustify width x xs = xs ++ replicate (width - length xs) x
-
-fitToWidth :: Int -> String -> String
-fitToWidth width = intercalate "\n" . splitEvery width . ljustify width ' '
-
-makeFittedColumnView :: [(Vty.Attr, Int)] -> [[String]] -> Display a
-makeFittedColumnView attrsWidths table = makeColumnView attrs fittedTable
-  where
-    fittedTable = (map . zipWith fitToWidth) widths table
-    attrs = map fst attrsWidths
-    widths = map snd attrsWidths
+    f (attr, width) =
+      Placable.atPlace modifyMkSizedImage .
+      Align.to (Vector2 0 0.5) .
+      (Placable.atRequestedSize . SizeRange.atMinSize . Vector2.first . const) width .
+      TextView.make attr .
+      fitToWidth width
+      where
+        modifyMkSizedImage mkSizedImage size arg =
+          modifySizedImage size $ mkSizedImage size arg
+        modifySizedImage size =
+          flip mappend . TermImage.rect (Rect (pure 0) size) . first . const $ attr
 
 makeKeymapView :: (Vty.Attr, Int) -> (Vty.Attr, Int) -> Keymap k -> Display a
 makeKeymapView keyAttrWidth valueAttrWidth keymap =
-  makeFittedColumnView [keyAttrWidth, valueAttrWidth] .
+  makeColumnView [keyAttrWidth, valueAttrWidth] .
   map (\(x, y) -> [x, y]) .
   sortBy (comparing snd) .
   Map.toList . Map.map fst . keymapGroups $
