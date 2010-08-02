76aeb45de671f94ad0aeb69d097f0dde2f8323bb
Eyal Lotem <eyal.lotem@gmail.com>
2010-08-02 22:24:41 +0300

    Add List.Utils.removeAt

--- src/Data/List/Utils.hs
+++ src/Data/List/Utils.hs
@@ -1,10 +1,13 @@
 {-# OPTIONS -Wall -O2 #-}
 
-module Data.List.Utils(safeIndex, groupFst) where
+module Data.List.Utils(safeIndex, groupFst, removeAt) where
 
 import Data.Maybe    (listToMaybe)
 import Data.List     (groupBy)
 import Data.Function (on)
+
+removeAt :: Int -> [a] -> [a]
+removeAt n xs = take n xs ++ drop (n+1) xs
 
 safeIndex :: Integral ix => ix -> [a] -> Maybe a
 safeIndex n = listToMaybe . drop (fromIntegral n)
